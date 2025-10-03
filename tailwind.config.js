be245ccacd43b581e0ee89ac29ff55ddec837511
Aman Rajpoot <amanrajpoot5612@gmail.com>
2025-10-03 13:30:58 +0530

    feat: add initial configuration files and dependencies for Tailwind CSS and Prettier

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        tertiary: "var(--color-tertiary)",
+      },
+    },
+  },
+}
