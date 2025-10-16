363095e1293dd63c78807930f3eb64556f57954b
Aman Rajpoot <amanrajpoot5612@gmail.com>
2025-10-16 15:57:18 +0530

    refactor: update backend URL exports to use production environment variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,15 @@
 const backend_prod_url = import.meta.env.VITE_BACKEND_PROD_URL
 const backend_dev_url = import.meta.env.VITE_BACKEND_DEV_URL
 
-export const backend_url = `${backend_dev_url}/api/barter/v1`
+// export const backend_url = `${backend_dev_url}/api/barter/v1`
 
-export const backend_page_url = `${backend_dev_url}/api/page/v1`
+// export const backend_page_url = `${backend_dev_url}/api/page/v1`
 
-export const backend_url_media = `${backend_dev_url}/api/upload/v1`
+// export const backend_url_media = `${backend_dev_url}/api/upload/v1`
+
+
+export const backend_url = `${backend_prod_url}/api/barter/v1`
+
+export const backend_page_url = `${backend_prod_url}/api/page/v1`
+
+export const backend_url_media = `${backend_prod_url}/api/upload/v1`
